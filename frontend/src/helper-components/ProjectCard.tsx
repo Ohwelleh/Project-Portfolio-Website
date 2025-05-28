208edf36216823a42af3a5fc225ba972d4a0386a
Austin <austin7955@hotmail.com>
2025-05-28 15:11:02 -0700

    Made the project cards expand vertically

--- frontend/src/helper-components/ProjectCard.tsx
+++ frontend/src/helper-components/ProjectCard.tsx
@@ -14,11 +14,11 @@ function ProjectCard({
   return (
     <div className="project-card">
       <h1>Project 1</h1>
-      <div className="project-card-info">
-        <p>Information about Project 1</p>
-      </div>
-      <div className="project-card-btn-container">
-        <SocialButton social={testSocial} socialLink={testLink} />
+      <div className="project-card-grid">
+        <div className="project-card-info">
+          <p>Information about Project 1</p>
+          <SocialButton social={testSocial} socialLink={testLink} />
+        </div>
       </div>
     </div>
   );
